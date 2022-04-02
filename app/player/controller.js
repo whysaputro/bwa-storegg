9fe43ec0b7bbfe0b9865fe7bb505562819723086
Hendra Wahyu Saputro <hendrawahyu26@gmail.com>
2022-04-02 21:45:01 +0700

    add payment on detail voucher API

--- app/player/controller.js
+++ app/player/controller.js
@@ -33,11 +33,18 @@ module.exports = {
         .populate('nominals')
         .populate('user', '_id name phoneNumber')
 
+      const payment = await Payment.find().populate('banks');
+
       if (!voucher) {
         return res.status(404).json({ message: "voucher game tidak ditemukan.!" })
       }
 
-      res.status(200).json({ data: voucher })
+      res.status(200).json({ 
+        data: {
+          detail: voucher,
+          payment
+        } 
+      })
 
     } catch (err) {
 
